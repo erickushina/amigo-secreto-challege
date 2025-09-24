707a80060a1e733085872fcaa965ad8572631a47
erickushina <erickushina2200@gmail.com>
2025-09-23 22:39:56 -0500

    Agrego funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+function limpiarCaja() {
+    let valorCaja = document.getElementById('amigo');
+    valorCaja.value = '';
+    return;
+}
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+
+    if(amigo == '') {
+        alert('Por favor, inserte un nombre');
+    } else {
+        listaDeAmigos.push(amigo);
+        limpiarCaja();
+    };
+    return;
+}
